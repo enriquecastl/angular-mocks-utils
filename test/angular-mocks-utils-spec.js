b9492eb6ee0496c7cc0a011cc71d1ed8abd226c8
Enrique Alcantara <enriquealc27@gmail.com>
2014-03-15 15:26:17 -0400

    allow to define filters and directives without passing factoryProviders as parameters

--- test/angular-mocks-utils-spec.js
+++ test/angular-mocks-utils-spec.js
@@ -1,12 +1,15 @@
 describe('angular mock utils', function() {
 
-    var filterProvider,
-        provider;
+    var $filterProvider,
+        $provide;
 
-    beforeEach(function(){
-        filterProvider = jasmine.createSpyObj('filterProvider', ['register']);
-        provider = jasmine.createSpyObj('provider', ['constant']);
-    });
+    beforeEach(module(function(_$filterProvider_, _$provide_) {
+        $filterProvider = _$filterProvider_;
+        $provide = _$provide_;
+
+        spyOn($filterProvider, 'register').and.callThrough();
+        spyOn($provide, 'constant').and.callThrough();
+    }));
 
     it('extends angular.mock with angular mock utils api', function() {
         [
@@ -19,47 +22,69 @@ describe('angular mock utils', function() {
         });
     });
 
-    it('registers a filter in the filter provider', function() {
-        var fakeFilter = angular.mock.fakeFilter(filterProvider, 'newFilter');
-        expect(filterProvider.register).toHaveBeenCalledWith('newFilter',
-            fakeFilter);
+    it('registers a filter in the filterProvider', function() {
+        angular.mock.fakeFilter('newFilter');
+
+        testExpectations(function() {
+            expect($filterProvider.register)
+            .toHaveBeenCalledWith('newFilter', jasmine.any(Function));
+        });
     });
 
     it('does not register a filter since it has an invalid name', function() {
-        angular.mock.fakeFilter(filterProvider, '');
-        expect(filterProvider.register).not.toHaveBeenCalled();
+        angular.mock.fakeFilter('');
+
+        testExpectations(function() {
+            expect($filterProvider.register).not.toHaveBeenCalled();
+        });
     });
 
     it('registers a list of filters', function() {
         var filters = ['filter1', 'filter2'];
 
-        angular.mock.fakeFilters(filterProvider, filters);
+        angular.mock.fakeFilters(filters);
 
-        filters.forEach(function(filter){
-            expect(filterProvider.register)
-            .toHaveBeenCalledWith(filter, jasmine.any(Function));
+        testExpectations(function() {
+            filters.forEach(function(filter){
+                expect($filterProvider.register)
+                .toHaveBeenCalledWith(filter, jasmine.any(Function));
+            });
         });
     });
 
-    it('registers a directive with the angular provider', function() {
-        var fakeDirective = angular.mock.fakeDirective(provider, 'directive');
-        expect(provider.constant).toHaveBeenCalledWith('directiveDirective',
-            fakeDirective);
+    it('registers a directive with the angular $provide', function() {
+        angular.mock.fakeDirective('directive');
+
+        testExpectations(function() {
+            expect($provide.constant).toHaveBeenCalledWith('directiveDirective',
+                jasmine.any(Function));
+        });
     });
 
     it('does not register a directive since it has an invalid name', function() {
-        angular.mock.fakeDirective(provider, null);
-        expect(provider.constant).not.toHaveBeenCalled();
+        angular.mock.fakeDirective(null);
+
+        testExpectations(function() {
+            expect($provide.constant).not.toHaveBeenCalled();
+        });
     });
 
     it('registers a list of directives', function() {
         var directives = ['directive1', 'directive2'];
 
-        angular.mock.fakeDirectives(provider, directives);
+        angular.mock.fakeDirectives(directives);
 
-        directives.forEach(function(directive){
-            expect(provider.constant)
-            .toHaveBeenCalledWith(directive+'Directive', jasmine.any(Function));
+        testExpectations(function() {
+            directives.forEach(function(directive){
+                expect($provide.constant).toHaveBeenCalledWith(directive+'Directive',
+                    jasmine.any(Function));
+            });
         });
     });
+
+    function testExpectations(test) {
+        inject(function(){
+            test();
+        });
+    }
 });
